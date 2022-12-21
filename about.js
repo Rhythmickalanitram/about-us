28096d26030b8a91bb4a9059bfadcf3fe1c1796c
Rhythmickalanitram <118746733+Rhythmickalanitram@users.noreply.github.com>
2022-12-21 21:10:58 +0530

    done

---
+++ about.js
@@ -0,0 +1,86 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".course-slider", {
+   spaceBetween: 20,
+   grabCursor:true,
+   loop:true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   breakpoints: {
+      540: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+var swiper = new Swiper(".teachers-slider", {
+   spaceBetween: 20,
+   grabCursor:true,
+   loop:true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   breakpoints: {
+      540: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+   spaceBetween: 20,
+   grabCursor:true,
+   loop:true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   breakpoints: {
+      540: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
